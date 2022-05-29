f166cd81ba61e2871e9c89f5e84b05667edeaa64
Kirill Dubasov <83347726+kdubasov@users.noreply.github.com>
2022-05-29 15:36:33 +0300

    Update Navbar.js

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,8 +3,8 @@ class Navbar{
         const html = `
             <div class="navbar-top">
                 <span>
-                    <div class="a-container "><a href="http://localhost:63342/uchiru/index.htm?_ijt=g2v7m5caov1nfoieli010hkno6&_ij_reload=RELOAD_ON_SAVE">Все котики</a></div>
-                    <div class="a-container"><a href="http://localhost:63342/uchiru/likesPage.htm?_ijt=gbt04an992vqlqjghoqoo0v2h2&_ij_reload=RELOAD_ON_SAVE">Любимые котики</a></div>
+                    <div class="a-container "><a href="https://kdubasov.github.io/uchirutest.github.io/">Все котики</a></div>
+                    <div class="a-container"><a href="https://kdubasov.github.io/uchirutest.github.io/likesPage.htm">Любимые котики</a></div>
                </span>
             </div>
         `;
